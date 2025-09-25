e1acf61d1975b6d1a745362a68f35fc995266f46
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-25 13:31:14 -0400

    Added bottle input

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+    int bottles;
     Scanner input = new Scanner(System.in);
+    
+    System.out.println("Please enter a number of bottles of beers:");
+    bottles = input.nextInt();
     input.close();
   }
 }
