a0c9449e9db9ee2eb028cbb544cbff5018ffac41
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-25 12:42:58 -0400

    Added coin flip and checks if heads

--- CountFlips.java
+++ CountFlips.java
@@ -2,8 +2,12 @@ public class CountFlips {
   public static void main(String[] args) {
     Coin myCoin = new Coin();
     int heads = 0, tails = 0;
-    
+  
     for(int x = 0; x != 100; x++){
+      myCoin.flip();
+      if(myCoin.isHeads()){
+        
+      }
     }
   }
 }
