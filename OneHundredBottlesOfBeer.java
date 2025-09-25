beaefdc7de875d3317a6d2be21ad5a99c385a11c
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-25 12:54:20 -0400

    Added scanner

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    input.close();
   }
 }
