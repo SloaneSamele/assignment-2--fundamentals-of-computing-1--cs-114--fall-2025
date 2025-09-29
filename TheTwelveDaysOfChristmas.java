e2a6954c3eff5e32d12f1309d6e947df2b963dfd
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-29 10:19:49 -0400

    Formatted verses into switch

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -1,19 +1,35 @@
 public class TheTwelveDaysOfChristmas {
   public static void main(String[] args) {
     for(int x = 1; x <= 12; x++){
-    System.out.println("Tweleve drummers drumming,");
-    System.out.println("Eleven pipers piping,");
-    System.out.println("Ten lords a-leaping,");
-    System.out.println("Nine ladies dancing,");
-    System.out.println("Eight maids a-milking,");
-    System.out.println("Seven swans a-swiming,");
-    System.out.println("Six geese a-laying,");
-    System.out.println("Five golden rings,");
-    System.out.println("Four calling birds,");
-    System.out.println("Three french hens,");
-    System.out.println("Two turtle doves, and");
-    System.out.println("A partrigde in a pear tree.");
-    System.out.println(x);
+      switch(x){
+        case 12:
+          System.out.println("Tweleve drummers drumming,");
+
+        case 11:
+          System.out.println("Eleven pipers piping,");
+        
+        case 10:
+          System.out.println("Ten lords a-leaping,");
+        
+        case 9:
+          System.out.println("Nine ladies dancing,");
+        case 8:
+          System.out.println("Eight maids a-milking,");
+        case 7:
+          System.out.println("Seven swans a-swiming,");
+        case 6:
+          System.out.println("Six geese a-laying,");
+        case 5:
+          System.out.println("Five golden rings,");
+        case 4:
+          System.out.println("Four calling birds,");
+        case 3:
+          System.out.println("Three french hens,");
+        case 2:
+          System.out.println("Two turtle doves, and");
+        case 1:
+          System.out.println("A partrigde in a pear tree.\n");
+      }
     }
   }
 }
