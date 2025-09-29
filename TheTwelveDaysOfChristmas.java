7349e274d2cfa5e1594085a8ce054be7b8b9cff3
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-29 10:21:44 -0400

    Added the first line

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -1,6 +1,18 @@
 public class TheTwelveDaysOfChristmas {
   public static void main(String[] args) {
     for(int x = 1; x <= 12; x++){
+      if(x == 1){
+        System.out.println("On the "+ x + "st day of Christmas, my true love gave to me");
+      }
+      else if(x == 2){
+        System.out.println("On the "+ x + "nd day of Christmas, my true love gave to me");
+      }
+      else if(x == 3){
+        System.out.println("On the "+ x + "rd day of Christmas, my true love gave to me");
+      }
+      else{
+      System.out.println("On the "+ x + "th day of Christmas, my true love gave to me");
+      }
       switch(x){
         case 12:
           System.out.println("Tweleve drummers drumming,");
