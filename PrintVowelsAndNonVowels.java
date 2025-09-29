b4253c3558f73527e28a9928605e804390a0854f
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-29 10:24:41 -0400

    Imported Scanner

--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
 public class PrintVowelsAndNonVowels {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    input.close();
+
   }
 }
