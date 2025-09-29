e425274110c70f9ff211cc1a1ac924298953bb2a
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-29 10:45:23 -0400

    Added counters and took in input

--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -1,7 +1,13 @@
 import java.util.Scanner;
 public class PrintVowelsAndNonVowels {
   public static void main(String[] args) {
+    String message;
+    int a = 0,e = 0,i = 0,o = 0,u = 0,other= 0;
     Scanner input = new Scanner(System.in);
+    
+    System.out.println("--Vowel Counter--");
+    System.out.print("Please enter a lowercase  message: ");
+    message = input.nextLine();
     input.close();
 
   }
