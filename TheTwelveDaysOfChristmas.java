b4a596bfedaaacc1b2f6f72e892fe917fb35e8ec
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-29 09:50:35 -0400

    Added verses

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -1,4 +1,16 @@
 public class TheTwelveDaysOfChristmas {
   public static void main(String[] args) {
+    System.out.println("Tweleve drummers drumming,");
+    System.out.println("Eleven pipers piping,");
+    System.out.println("Ten lords a-leaping,");
+    System.out.println("Nine ladies dancing,");
+    System.out.println("Eight maids a-milking,");
+    System.out.println("Seven swans a-swiming,");
+    System.out.println("Six geese a-laying,");
+    System.out.println("Five golden rings,");
+    System.out.println("Four calling birds,");
+    System.out.println("Three french hens,");
+    System.out.println("Two turtle doves, and");
+    System.out.println("A partrigde in a pear tree.");
   }
 }
